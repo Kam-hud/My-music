578d0a88952e1531eef247e385bda0acc59b7a75
Kam <14308429+kamweb@user.noreply.gitee.com>
2025-12-03 10:38:32 +0800

    router

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
     {
         path: '/',
-        // redirect: '/recommended'
-        redirect: '/winnow'
+        redirect: '/recommended'
     },
     {
         path: '/my',
